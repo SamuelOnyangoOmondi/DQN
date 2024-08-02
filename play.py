16c3585da2333a7f889b7da183b8f0e3ab79126b
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2024-08-02 17:43:13 +0300

    Update play.py

--- play.py
+++ play.py
@@ -20,11 +20,13 @@ dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 dqn.load_weights('dqn_weights.h5f')
 
 # Play the game
-observation = env.reset()
+observation, _ = env.reset()
+observation = np.array([observation])  # Add batch dimension for model compatibility
 done = False
 while not done:
     action = dqn.forward(observation)
     observation, reward, done, info = env.step(action)
+    observation = np.array([observation])  # Reformat for next prediction
     env.render()
 
 env.close()
