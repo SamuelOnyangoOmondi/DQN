ee30ae06a42d5643130082eed8416b35f1945808
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2024-08-02 14:00:26 +0300

    Create test_env.py

---
+++ test_env.py
@@ -0,0 +1,13 @@
+from Sam'scase_env import HospitalEnv
+
+env = HospitalEnv()
+env.reset()
+done = False
+
+while not done:
+    action = env.action_space.sample()
+    obs, reward, done, info = env.step(action)
+    env.render()
+    print(f"Step: {action}, State: {obs}, Reward: {reward}")
+
+env.close()
