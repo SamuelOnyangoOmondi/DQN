f8092abe4287ca5f923171649f0140f13b591a3e
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2024-08-02 17:43:42 +0300

    Update test_env.py

--- test_env.py
+++ test_env.py
@@ -1,12 +1,13 @@
 from Samcase_env import HospitalEnv
 
 env = HospitalEnv()
-env.reset()
+observation, _ = env.reset()
 done = False
 
 while not done:
     action = env.action_space.sample()
     obs, reward, done, info = env.step(action)
+    obs = np.array([obs])  # Ensure proper shape if passed to any model
     env.render()
     print(f"Step: {action}, State: {obs}, Reward: {reward}")
 
