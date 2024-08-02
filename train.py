1752648f414e8ec06829a6b9a49ea199fc319ded
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2024-08-02 17:13:25 +0300

    Update train.py

--- train.py
+++ train.py
@@ -10,11 +10,6 @@ from Samcase_env import HospitalEnv
 # Setup the environment
 env = HospitalEnv()
 
-# Make sure the observation space is properly initialized in HospitalEnv
-# For example, the observation_space could be something like this in HospitalEnv:
-# self.observation_space = spaces.Box(low=0, high=4, shape=(2,), dtype=np.int32)
-# which represents a 5x5 grid (0 to 4, both x and y coordinates).
-
 # Build the model
 model = Sequential([
     Flatten(input_shape=(1,) + env.observation_space.shape),
