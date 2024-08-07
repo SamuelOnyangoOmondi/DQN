5711d7939464eed3b2a91191c853ab07334b425d
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2024-08-08 00:22:16 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,32 +1,32 @@
-import gym
 import numpy as np
-from Samcase_env import HospitalEnv
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.optimizers import Adam
 from rl2.agents.dqn import DQNAgent
-from rl2.policy import BoltzmannQPolicy
+from rl2.policy import EpsGreedyQPolicy
 from rl2.memory import SequentialMemory
+from Samcase_env import HospitalEnv
 
 # Setup the environment
 env = HospitalEnv()
 
-# Model Configuration
+# Build the model
 model = Sequential([
     Flatten(input_shape=(1,) + env.observation_space.shape),
-    Dense(24, activation='relu'),
-    Dense(24, activation='relu'),
+    Dense(16, activation='relu'),
+    Dense(16, activation='relu'),
+    Dense(16, activation='relu'),
     Dense(env.action_space.n, activation='linear')
 ])
 
-# Agent Configuration
+# Configure and compile the agent
 memory = SequentialMemory(limit=50000, window_length=1)
-policy = BoltzmannQPolicy()
+policy = EpsGreedyQPolicy()
 dqn = DQNAgent(model=model, nb_actions=env.action_space.n, memory=memory, nb_steps_warmup=10, target_model_update=1e-2, policy=policy)
 dqn.compile(Adam(learning_rate=1e-3), metrics=['mae'])
 
-# Training
+# Train the agent
 dqn.fit(env, nb_steps=5000, visualize=False, verbose=2)
 
-# Save Model Weights
-dqn.save_weights('dqn_hospital_weights.h5f', overwrite=True)
+# Save weights
+dqn.save_weights('dqn_weights.h5f', overwrite=True)
