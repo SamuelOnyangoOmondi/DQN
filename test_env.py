81c102ae4d7488a7b365aa279579742647abe602
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2024-08-10 00:10:19 +0300

    Update test_env.py

--- test_env.py
+++ test_env.py
@@ -1,19 +1,16 @@
-import gym
 from plastech_env import PlasTechEnv
 
 def test_environment():
     env = PlasTechEnv()
     env.reset()
-
     print("Testing environment initialization...")
     initial_state = env.reset()
     print("Initial state:", initial_state)
-
     for _ in range(10):
         action = env.action_space.sample()
         next_state, reward, done, info = env.step(action)
         print(f"Action taken: {action} -> Next state: {next_state}, Reward: {reward}, Done: {done}")
-
+    env.render()
     env.close()
     print("Environment closed successfully.")
 
